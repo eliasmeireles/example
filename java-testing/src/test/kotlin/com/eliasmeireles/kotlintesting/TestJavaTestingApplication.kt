33e06aab82e196f3a9d07663f0c214e098d48e73
Elias Miereles <eliasmflilico@gmail.com>
2025-02-08 12:29:45 -0300

    chore: based project name changed

--- java-testing/src/test/kotlin/com/eliasmeireles/kotlintesting/TestJavaTestingApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.eliasmeireles.javatesting
-
-import org.springframework.boot.fromApplication
-import org.springframework.boot.with
-
-
-fun main(args: Array<String>) {
-    fromApplication<JavaTestingApplication>().with(TestcontainersConfiguration::class).run(*args)
-}
